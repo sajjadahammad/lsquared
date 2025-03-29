b7b2f1e48eddd903a4d084d68a71b68410436e77
sajjadahammed <sajjad08@protonmail.com>
2025-03-29 14:02:29 +0530

    Add New Event dialog and AllScheduled component; update Calendar page layout

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -4,6 +4,11 @@ import {
     TabsList,
     TabsTrigger,
   } from "@/components/ui/tabs"
+  import { Button } from "@/components/ui/button"
+
+import { Calendar1, ClipboardList, Ellipsis, Filter, MessagesSquare, Plus, Search, Star } from "lucide-react"
+import NewEvent from "@/components/NewEvent"
+import AllScheduled from "@/components/AllScheduled"
 
 export default function Calendar() {
     return (
@@ -11,13 +16,26 @@ export default function Calendar() {
             <h1 className='text-3xl'>Calendar</h1>
             <p className='text-sm text-zinc-600'>Stay Organized and On Track with Your Personalized Calendar</p>
             <Tabs defaultValue="scheduled" >
-                <TabsList>
-                    <TabsTrigger value="scheduled">All Scheduled</TabsTrigger>
-                    <TabsTrigger value="events">Events</TabsTrigger>
-                    <TabsTrigger value="meetings">Meetings</TabsTrigger>
-                    <TabsTrigger value="taskreminders">Task Reminders</TabsTrigger>
-                </TabsList>
-                <TabsContent value="scheduled">Make changes to your account here.</TabsContent>
+                <div className="flex items-center justify-between mb-4 border-b ">
+                    <TabsList>
+                        <TabsTrigger value="scheduled"><Calendar1/> All Scheduled</TabsTrigger>
+                        <TabsTrigger value="events"><Star/> Events</TabsTrigger>
+                        <TabsTrigger value="meetings"><MessagesSquare/>Meetings</TabsTrigger>
+                        <TabsTrigger value="taskreminders"><ClipboardList/>Task Reminders</TabsTrigger>
+                    </TabsList>
+                    <div className="flex items-center gap-2 pb-5">
+                        <div className="relative w-24">
+                            <Search size={12} className="absolute left-2 top-1/2 transform -translate-y-1/2"/>
+                            <input type="text" className="border rounded-sm w-full text-xs py-1.5 ps-8" placeholder="Search.." />
+                        </div>
+                        <Button className="text-xs" variant='outline'><Filter size={12}/> Filter</Button>
+                        <Button className="text-xs" variant='outline'><Ellipsis/></Button>
+                        <NewEvent/>
+                    </div>
+                </div>
+                <TabsContent value="scheduled">
+                    <AllScheduled/>
+                </TabsContent>
                 <TabsContent value="events">Change your password here.</TabsContent>
                 <TabsContent value="meetings">Meetings</TabsContent>
                 <TabsContent value="taskreminders">Taskreminders</TabsContent>
